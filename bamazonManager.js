3b2cb690621d5d4cbab95f8fc96e45c7568119c4
Monica Mantilla <monicamantilla@MacBook-Pro-2.local>
2019-06-04 17:15:40 -0400

    manager working

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,3 +20,55 @@ connection.connect(function(err) {
   if (err) throw err;
 
 });
+
+var table = new Table({
+    head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
+  , colWidths: [10, 20, 20, 10, 20], 
+  colAligns: ["center", "left", "left", "center", "center"], 
+  style: {
+    head: ["pink"], 
+    compact: true
+}
+});
+
+
+var promptManger = function(){
+    inquirer.prompt({
+        type: 'list',
+        name: 'option',
+        message: 'Please select an option:',
+        choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
+    }).then(function(selection){
+
+        if(selection.option  === 'View Products for Sale'){
+            connection.query("SELECT * FROM products", function(err, res) {
+                if (err) throw err;
+                console.log("Display Inventory: " + "\n" + "--------------") 
+            })
+        }
+        if(selection.option === 'View Low Inventory'){
+
+        }
+
+    })
+}
+
+promptManger();
+
+
+// var viewProducts = function(){
+
+// }
+
+// var viewLowInventory = function(){
+
+// }
+
+// var addToInventory = function(){
+
+// }
+
+// var addNewProduct = function(){
+
+// }
+
