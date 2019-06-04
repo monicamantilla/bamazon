3bd5851de050dae66b4a94dc6a632a3a17ae3507
Monica Mantilla <monicamantilla@MacBook-Pro-2.local>
2019-06-04 18:04:25 -0400

    low inventory working

--- bamazonManager.js
+++ bamazonManager.js
@@ -18,57 +18,78 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
   if (err) throw err;
-
-});
-
-var table = new Table({
-    head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
-  , colWidths: [10, 20, 20, 10, 20], 
-  colAligns: ["center", "left", "left", "center", "center"], 
-  style: {
-    head: ["pink"], 
-    compact: true
-}
+  promptManager();
 });
 
 
-var promptManger = function(){
+
+var promptManager = function(){
     inquirer.prompt({
         type: 'list',
         name: 'option',
         message: 'Please select an option:',
         choices: ['View Products for Sale', 'View Low Inventory', 'Add to Inventory', 'Add New Product'],
-    }).then(function(selection){
-
-        if(selection.option  === 'View Products for Sale'){
-            connection.query("SELECT * FROM products", function(err, res) {
-                if (err) throw err;
-                console.log("Display Inventory: " + "\n" + "--------------") 
-            })
+    }).then(function(answer){
+        switch (answer.option){
+            case "View Products for Sale":
+            display();
+            break;
+            case "View Low Inventory":
+            viewLow();
+            break;
+            case "Add to Inventory":
+            addInventory();
+            break;
+            case "Add New Product":
+            addNewProduct();
+            break;
+            case "Quit":
+            quit();
         }
-        if(selection.option === 'View Low Inventory'){
+    });
+};
+    
+function display(){
+    connection.query("SELECT * FROM products", function(err, res) {
+        if (err) throw err;
 
+        var table = new Table({
+            head: ['Item ID', 'Product Name', 'Department Name', 'Price', 'Stock Quantity']
+            , colWidths: [10, 20, 20, 10, 20], 
+            colAligns: ["center", "left", "left", "center", "center"], 
+            style: {
+            head: ["pink"], 
+            compact: true
         }
+        });
+        for(var i = 0; i < res.length; i++){
+            table.push ([res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+            }
+        console.log(table.toString());
+        promptManager();
+    });
+};
 
-    })
-}
 
-promptManger();
+function viewLow(){
 
+  connection.query("SELECT * FROM products WHERE stock_quantity <= 50", function(err, res) {
 
-// var viewProducts = function(){
+        if (err) throw err;
 
-// }
-
-// var viewLowInventory = function(){
-
-// }
-
-// var addToInventory = function(){
-
-// }
-
-// var addNewProduct = function(){
-
-// }
-
+        var table = new Table ({
+            head: ['Product ID', 'Product Name', 'Department', 'Price', 'Quantity']
+            , colWidths: [10, 10, 20, 10, 10],
+            colAligns: ["center", "left", "left", "right", "center"],
+            style: {
+                head: ["blue"],
+                compact: true
+            }
+        });
+        for(let i = 0; i < res.length; i++) {
+            table.push([res[i].id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity])
+        }
+        console.log(table.toString());
+        promptManager(); 
+    });
+};
